1eb4306b1ec0e6a1a64cb58ce611dda1f8dc42eb
Unis Mohammed <adanunis@gmail.com>
2024-09-10 10:27:10 +0100

    added venv to gitignore added database name to database connection file.

--- lib/database_connection.py
+++ lib/database_connection.py
@@ -10,8 +10,8 @@ from psycopg.rows import dict_row
 # That's why we have provided it!
 class DatabaseConnection:
     # VVV CHANGE BOTH OF THESE VVV
-    DEV_DATABASE_NAME = "DEFAULT_MAKERSBNB_PROJECT"
-    TEST_DATABASE_NAME = "DEFAULT_MAKERSBNB_PROJECT_TEST"
+    DEV_DATABASE_NAME = "makersbnb_db"
+    TEST_DATABASE_NAME = "makersbnb_test_db"
 
     def __init__(self, test_mode=False):
         self.test_mode = test_mode
