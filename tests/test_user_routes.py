08bc57ef4bb29cf4fe94f938476bd21bcc25185b
Lachlan <lockie_4_hockey@hotmail.com>
2024-09-12 13:40:14 +0100

    add homepage button to my-listings

--- tests/test_user_routes.py
+++ tests/test_user_routes.py
@@ -16,45 +16,6 @@ to see all the users
 #                             'User(3, host1@example.com, password3, host1)\n' \
 #                             'User(4, host2@example.com, password4, host2)'
     
-'''
-As a user, when I click the sign up button with my details
-an account is created
-'''
-
-def test_sign_up_on_page(web_client, test_web_address, db_connection, page: Page):
-    db_connection.seed('seeds/makersbnb.sql')
-    connection = db_connection
-    repo = UserRepository(connection)
-    page.goto(f'http://{test_web_address}/create-account')
-    page.fill("input[name='username']", 'test1')
-    page.fill("input[name='password']", 'password1')
-    page.fill("input[name='email']", 'user5@example.com')
-    page.locator('.createaccount').click()
-    assert repo.all() == [
-        User(1, 'guest1@example.com', 'password1', 'guest1'),
-        User(2, 'guest2@example.com', 'password2', 'guest2'),
-        User(3, 'host1@example.com', 'password3', 'host1'),
-        User(4, 'host2@example.com', 'password4', 'host2'),
-        User(5, 'user5@example.com', 'password1', 'test1')
-    ]
-
-def test_account_exists_go_to_sign_in(test_web_address, db_connection, page: Page):
-    db_connection.seed('seeds/makersbnb.sql')
-    page.goto(f'http://{test_web_address}/create-account')
-    page.locator('.signin').click()
-    h2 = page.locator('h2')
-    expect(h2).to_have_text('Sign In')
-
-def test_create_account_goes_to_sign_in(test_web_address, db_connection, page: Page):
-    db_connection.seed('seeds/makersbnb.sql')
-    page.goto(f'http://{test_web_address}/create-account')
-    page.fill("input[name='username']", 'test1')
-    page.fill("input[name='password']", 'password1')
-    page.fill("input[name='email']", 'user5@example.com')
-    page.locator('.createaccount').click()
-    h2 = page.locator('h2')
-    expect(h2).to_have_text('Sign In')
-
 
 
 
--- tests/test_view_bookings.py
+++ tests/test_view_bookings.py
@@ -30,3 +30,17 @@ def test_view_bookings_with_details(test_web_address, db_connection, page: Page)
     expect(p_list.nth(0)).to_have_text("Beach House\n150")
     expect(p_list.nth(1)).to_have_text("Mountain Cabin\n120")
 
+'''
+As a user on the my booking page
+I can click on a homepage button
+so that I go back to the homepage
+'''
+
+def test_button_from_my_bookings_to_homepage(test_web_address, page):
+    page.goto(f'http://{test_web_address}/sign-in')
+    page.fill("input[name='email']", 'guest1@example.com')
+    page.fill("input[name='password']", 'password1')
+    page.locator('.signin').click()
+    page.goto(f'http://{test_web_address}/my-bookings')
+    page.locator('.homepage').click()
+    assert page.url == f'http://{test_web_address}/index'
