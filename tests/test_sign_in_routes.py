e6485811f9686229658c5e16e2f7600a99cfbfe3
Lachlan <lockie_4_hockey@hotmail.com>
2024-09-12 13:44:53 +0100

    add hompage button to sign-in page

--- tests/test_sign_in_routes.py
+++ tests/test_sign_in_routes.py
@@ -39,3 +39,14 @@ def test_can_get_to_create_account_from_sign_in_page(test_web_address, page):
     page.goto(f'http://{test_web_address}/sign-in')
     page.locator('.createaccount').click()
     assert page.url == f'http://{test_web_address}/create-account'
+
+'''
+When I am on the sign-in page
+I can click on a homepage button
+So that I can go back to the homepage (/index)
+'''
+
+def test_homepage_button_from_sign_in(test_web_address, page):
+    page.goto(f'http://{test_web_address}/sign-in')
+    page.locator('.homepage').click()
+    assert page.url == f'http://{test_web_address}/index'
