55b6137ddf1f5e6c19d15cbf9a95c5cfbfe74168
Joe Winter <joewinter333@gmail.com>
2024-09-12 11:45:37 +0100

    when host is signed in they can create a space

--- routes/space_routes.py
+++ routes/space_routes.py
@@ -6,21 +6,6 @@ from lib.space import Space
 # function to import the route in app.py
 def apply_space_routes(app):   
 
-
-    # POST /create-space
-    # Creates a space
-    
-    @app.route('/create-space', methods=['POST'])
-    def create_space():
-        connection = get_flask_database_connection(app)
-        repository = SpaceRepository(connection)
-        space = Space(None, request.form['name'], request.form['description'], request.form['price_per_night'], request.form['availability_start'], request.form['availability_end'], request.form['user_id']) 
-        space = repository.create(space)
-        return "Space added successfully"
-    
-    # GET /all-spaces
-    # Lists all spaces in the database
-
     @app.route('/all-spaces', methods=['GET'])
     def get_all_spaces():
         connection = get_flask_database_connection(app)
