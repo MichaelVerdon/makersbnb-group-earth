ef63ad67993baa60fdd6b14fc68b08cd6946878f
Joe Winter <joewinter333@gmail.com>
2024-09-13 12:13:30 +0100

    changed class to id in html

--- tests/test_host_listings_route.py
+++ tests/test_host_listings_route.py
@@ -14,11 +14,11 @@ def test_show_spaces_for_host(test_web_address, db_connection, page: Page):
     page.fill("input[name='password']", 'password3')
     page.locator('.signin').click()
     page.goto(f'http://{test_web_address}/host-listings')
-    expect(page.locator('.name')).to_have_text('Name: Beach House')
-    expect(page.locator('.description')).to_have_text('Description: A beautiful beach house with ocean view.')
-    expect(page.locator('.price')).to_have_text('Price per Night: 150')
-    expect(page.locator('.start')).to_have_text('Availability Start: 2024-09-15')
-    expect(page.locator('.end')).to_have_text('Availability End: 2024-09-30')
+    expect(page.locator('#name')).to_have_text('Name: Beach House')
+    expect(page.locator('#description')).to_have_text('Description: A beautiful beach house with ocean view.')
+    expect(page.locator('#price')).to_have_text('Price per Night: 150')
+    expect(page.locator('#start')).to_have_text('Availability Start: 2024-09-15')
+    expect(page.locator('#end')).to_have_text('Availability End: 2024-09-30')
 
 '''
 When I am on listings, 
@@ -59,7 +59,7 @@ def test_new_create_page(test_web_address, db_connection, page: Page):
     page.fill("input[name='password']", 'password3')
     page.locator('.signin').click()
     page.goto(f'http://{test_web_address}/host-listings')
-    page.locator('.createspace').click()
+    page.locator('#createspace').click()
     expect(page.locator('.page-title')).to_have_text('Create new space')
 """
 when the user  create a new space the new space is visible on the listings page 
@@ -77,12 +77,12 @@ def test_user_create_space(test_web_address, db_connection, page: Page):
     page.fill("input[name='price_per_night']", "300")
     page.fill("input[name='availability_start']", "2024-09-27")
     page.fill("input[name='availability_end']","2024-10-15")
-    page.locator('.createspace').click()
-    expect(page.locator('.name').nth(1)).to_have_text('Name: testName')
-    expect(page.locator('.description').nth(1)).to_have_text('Description: testDescription')
-    expect(page.locator('.price').nth(1)).to_have_text('Price per Night: 300')
-    expect(page.locator('.start').nth(1)).to_have_text('Availability Start: 2024-09-27')
-    expect(page.locator('.end').nth(1)).to_have_text('Availability End: 2024-10-15')
+    page.locator('#createspace').click()
+    expect(page.locator('#name').nth(1)).to_have_text('Name: testName')
+    expect(page.locator('#description').nth(1)).to_have_text('Description: testDescription')
+    expect(page.locator('#price').nth(1)).to_have_text('Price per Night: 300')
+    expect(page.locator('#start').nth(1)).to_have_text('Availability Start: 2024-09-27')
+    expect(page.locator('#end').nth(1)).to_have_text('Availability End: 2024-10-15')
 
 def test_user_updates_space_name(test_web_address, db_connection, page: Page):
     db_connection.seed('seeds/makersbnb.sql')
@@ -91,11 +91,12 @@ def test_user_updates_space_name(test_web_address, db_connection, page: Page):
     page.fill("input[name='password']", 'password3')
     page.locator('.signin').click()
     page.goto(f'http://{test_web_address}/host-listings')
-    page.locator('.update_1').click()
-    expect(page.locator('.name')).to_have_text('Beach House')
+    page.locator('#update_1').click()
+    page.screenshot(path='screenshot.png', full_page=True)
+    expect(page.locator('#name')).to_have_text('Beach House')
     page.fill("input[name='name']", 'Lake House')
-    page.locator('.update').click()
-    expect(page.locator('.name').nth(0)).to_have_text('Name: Lake House')
+    page.locator('#update').click()
+    expect(page.locator('#name').nth(0)).to_have_text('Name: Lake House')
 
 def test_user_updates_with_empty_field(test_web_address, db_connection, page: Page):
     db_connection.seed('seeds/makersbnb.sql')
@@ -104,10 +105,10 @@ def test_user_updates_with_empty_field(test_web_address, db_connection, page: Pa
     page.fill("input[name='password']", 'password3')
     page.locator('.signin').click()
     page.goto(f'http://{test_web_address}/host-listings')
-    page.locator('.update_1').click()
-    expect(page.locator('.name')).to_have_text('Beach House')
-    page.locator('.update').click()
-    expect(page.locator('.name').nth(0)).to_have_text('Name: Beach House')
+    page.locator('#update_1').click()
+    expect(page.locator('#name')).to_have_text('Beach House')
+    page.locator('#update').click()
+    expect(page.locator('#name').nth(0)).to_have_text('Name: Beach House')
 
 def test_user_updates_space_description(test_web_address, db_connection, page: Page):
     db_connection.seed('seeds/makersbnb.sql')
@@ -116,11 +117,11 @@ def test_user_updates_space_description(test_web_address, db_connection, page: P
     page.fill("input[name='password']", 'password3')
     page.locator('.signin').click()
     page.goto(f'http://{test_web_address}/host-listings')
-    page.locator('.update_1').click()
-    expect(page.locator('.description')).to_have_text('A beautiful beach house with ocean view.')
+    page.locator('#update_1').click()
+    expect(page.locator('#description')).to_have_text('A beautiful beach house with ocean view.')
     page.fill("input[name='description']", 'A beautiful lake house with lake view.')
-    page.locator('.update').click()
-    expect(page.locator('.description').nth(0)).to_have_text('Description: A beautiful lake house with lake view.')
+    page.locator('#update').click()
+    expect(page.locator('#description').nth(0)).to_have_text('Description: A beautiful lake house with lake view.')
 
 def test_user_updates_space_price(test_web_address, db_connection, page: Page):
     db_connection.seed('seeds/makersbnb.sql')
@@ -129,11 +130,11 @@ def test_user_updates_space_price(test_web_address, db_connection, page: Page):
     page.fill("input[name='password']", 'password3')
     page.locator('.signin').click()
     page.goto(f'http://{test_web_address}/host-listings')
-    page.locator('.update_1').click()
-    expect(page.locator('.price')).to_have_text('150')
+    page.locator('#update_1').click()
+    expect(page.locator('#price')).to_have_text('150')
     page.fill("input[name='price_per_night']", '300')
-    page.locator('.update').click()
-    expect(page.locator('.price').nth(0)).to_have_text('Price per Night: 300')
+    page.locator('#update').click()
+    expect(page.locator('#price').nth(0)).to_have_text('Price per Night: 300')
 
 def test_user_updates_space_availability_start(test_web_address, db_connection, page: Page):
     db_connection.seed('seeds/makersbnb.sql')
@@ -142,11 +143,11 @@ def test_user_updates_space_availability_start(test_web_address, db_connection, 
     page.fill("input[name='password']", 'password3')
     page.locator('.signin').click()
     page.goto(f'http://{test_web_address}/host-listings')
-    page.locator('.update_1').click()
-    expect(page.locator('.availability_start')).to_have_text('2024-09-15')
+    page.locator('#update_1').click()
+    expect(page.locator('#availability_start')).to_have_text('2024-09-15')
     page.fill("input[name='availability_start']", '2024-09-29')
-    page.locator('.update').click()
-    expect(page.locator('.start').nth(0)).to_have_text('Availability Start: 2024-09-29')
+    page.locator('#update').click()
+    expect(page.locator('#start').nth(0)).to_have_text('Availability Start: 2024-09-29')
 
 def test_user_updates_space_availability_end(test_web_address, db_connection, page: Page):
     db_connection.seed('seeds/makersbnb.sql')
@@ -155,11 +156,11 @@ def test_user_updates_space_availability_end(test_web_address, db_connection, pa
     page.fill("input[name='password']", 'password3')
     page.locator('.signin').click()
     page.goto(f'http://{test_web_address}/host-listings')
-    page.locator('.update_1').click()
-    expect(page.locator('.availability_end')).to_have_text('2024-09-30')
+    page.locator('#update_1').click()
+    expect(page.locator('#availability_end')).to_have_text('2024-09-30')
     page.fill("input[name='availability_end']", '2024-10-15')
-    page.locator('.update').click()
-    expect(page.locator('.end').nth(0)).to_have_text('Availability End: 2024-10-15')
+    page.locator('#update').click()
+    expect(page.locator('#end').nth(0)).to_have_text('Availability End: 2024-10-15')
 
 """
 When a user tries to update the end so its before the start date, they get an error
@@ -172,11 +173,11 @@ def test_user_updates_space_availability_end_when_end_is_before_start(test_web_a
     page.fill("input[name='password']", 'password3')
     page.locator('.signin').click()
     page.goto(f'http://{test_web_address}/host-listings')
-    page.locator('.update_1').click()
-    expect(page.locator('.availability_end')).to_have_text('2024-09-30')
+    page.locator('#update_1').click()
+    expect(page.locator('#availability_end')).to_have_text('2024-09-30')
     page.fill("input[name='availability_end']", '2024-08-15')
-    page.locator('.update').click()
-    expect(page.locator('.errormessage')).to_have_text("End date cannot be earlier than start date.")
+    page.locator('#update').click()
+    expect(page.locator('#errormessage')).to_have_text("End date cannot be earlier than start date.")
 
 """
 When a user tries to update the start so its after the end date, they get an error
@@ -189,11 +190,11 @@ def test_user_updates_space_availability_start_when_start_is_after_end(test_web_
     page.fill("input[name='password']", 'password3')
     page.locator('.signin').click()
     page.goto(f'http://{test_web_address}/host-listings')
-    page.locator('.update_1').click()
-    expect(page.locator('.availability_start')).to_have_text('2024-09-15')
+    page.locator('#update_1').click()
+    expect(page.locator('#availability_start')).to_have_text('2024-09-15')
     page.fill("input[name='availability_start']", '2024-10-29')
-    page.locator('.update').click()
-    expect(page.locator('.errormessage')).to_have_text("End date cannot be earlier than start date.")
+    page.locator('#update').click()
+    expect(page.locator('#errormessage')).to_have_text("End date cannot be earlier than start date.")
 
 """
 When I delete a space it is no longer visible
@@ -205,7 +206,7 @@ def test_user_delete_space(test_web_address, db_connection, page: Page):
     page.fill("input[name='password']", 'password3')
     page.locator('.signin').click()
     page.goto(f'http://{test_web_address}/host-listings')
-    page.locator('.delete_1').click()
+    page.locator('#delete_1').click()
     expect(page.locator('h2')).to_have_text('Your Listings')
-    expect(page.locator('.name')).to_have_count(0)
+    expect(page.locator('#name')).to_have_count(0)
 
